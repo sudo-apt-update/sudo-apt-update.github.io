6baf5f5f8a6d1fe50e9c9d91991dffe85b0a0e25
= <jumpingraspberries@gmail.com>
2020-08-28 14:35:35 -0400

    Added new pages, sections, and css and js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-window.onload = function(){
-document.getElementById("footer").innerHTML = "Page loaded  " + Date() + "; page last edited Wed Aug 26 2020";
+window.onload = function() {
+	document.getElementById("footer").innerHTML = "Page loaded  " + Date() + "; page last edited Fri Aug 28 2020";
+}
+
+var hidden = true;
+function toggle(id) {
+	var element = document.getElementById(id);
+	if (hidden) {
+		element.style.display = "block";
+		hidden = !hidden;
+	} else {
+		element.style.display = "none";
+		hidden = !hidden;
+	}
 }
