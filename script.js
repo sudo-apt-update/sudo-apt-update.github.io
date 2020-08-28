423f203699add990d48641f88498cd349212047a
= <jumpingraspberries@gmail.com>
2020-08-28 15:48:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	document.getElementById("footer").innerHTML = "Page loaded  " + Date() + "; page last edited Fri Aug 28 2020";
 }
 
-var hidden = true;
+var hidden = false;
 function toggle(id) {
 	var element = document.getElementById(id);
 	if (hidden) {
