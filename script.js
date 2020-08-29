2da1842aeab2a2edc3e46492ea840a4da3d4d199
= <jumpingraspberries@gmail.com>
2020-08-28 22:36:13 -0400

    Added programming and school sections, fixed footer
    Ruby, Java, Python, web dev suite, and my classes this year. Footer stays at bottom of page.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	document.getElementById("footer").innerHTML = "Page loaded  " + Date() + "; page last edited Fri Aug 28 2020";
 }
 
-var hidden = false;
+var hidden = true;
 function toggle(id) {
 	var element = document.getElementById(id);
 	if (hidden) {
