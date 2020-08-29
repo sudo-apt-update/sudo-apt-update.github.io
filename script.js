448d5bd9a8bf3d33995a9adeca85321ff946640e
= <jumpingraspberries@gmail.com>
2020-08-29 16:49:59 -0400

    Reformatted date and changed titles
    I put my name on just about everything.

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+var t = "AM";
+var d = new Date();
+var date = d.toDateString();
+
+function addZero(i) {
+	if (i < 10) {
+		i = "0" + i;
+	}
+	return i;
+}
+
+function amPM(j) {
+	if (j > 12) {
+		j -= 12;
+		t = "PM";
+	}
+	return j;
+}
+
+var h = addZero(amPM(d.getHours()));
+var time = " " + h + ":" + addZero(d.getMinutes()) + ":" + addZero(d.getSeconds());
+
 window.onload = function() {
-	document.getElementById("footer").innerHTML = "Page loaded  " + Date() + "; Site edited Sat Aug 29 2020";
+	document.getElementById("footer").innerHTML = "Page loaded  " + date + " " + time + " " + t + "; Site edited Sat Aug 29 2020";
 }
 
 var hidden = true;
